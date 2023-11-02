2a0eadfbdc99b501316262961193788e64f5ef83
tzuuuu <116798227+tzuuuu@users.noreply.github.com>
2023-11-02 18:32:07 +0800

    新增第一筆檔案

---
+++ 0920_ex1_年紀.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  7 15:32:50 2022
+
+@author: tzuuu
+"""
+
+name=str(input("Please input your name:"))
+age=int(input("Please input your age:"))
+if age>18:
+    print('Welcome '+name+',you are are greater than 18!')
+else:
+    print('you lose')
